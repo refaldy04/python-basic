5d3aa5c817c803a903ebad79e8442e69e06f4ffc
Refaldy Bagas Anggana <refaldybanggana@gmail.com>
2023-11-02 19:15:21 +0700

    print and variables

---
+++ 01-print-and-variables.py
@@ -0,0 +1,8 @@
+x = 'Refaldy'
+y = 'Bagas'
+
+print(f'{x} {y}')
+print('Hello World 1 \nHello World 2')
+# ip = input('What is the target ip?\n')
+# print(f'Scanning the target: {ip}')
+print('You are targeting ' + input('What ip would you like to target? '))
